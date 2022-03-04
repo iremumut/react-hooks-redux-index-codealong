efa656c8c85562ad7de74a79df56c4ca77d572b0
iremumut <irem2000.nur@gmail.com>
2022-03-04 18:52:02 +0300

    displays toDos

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import Todo from "./Todo";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+
+  const toDos = useSelector((state) => state.todos.entities);
+
+  return (
+    <ul>
+      <div>TodoContainer</div>
+      {toDos.map((toDo) => {
+        return <Todo text={toDo} key={toDo}></Todo>
+      })}
+
+      </ul>
+  );
 }
 
 export default TodoContainer;
